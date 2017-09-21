9112c5577902f712f6f24d365e8bfd78a9328419
Pranay Mehta <pmehta33@uic.edu>
2017-09-21 13:53:57 -0500

    Add files via upload

---
+++ googler.py
@@ -0,0 +1,17 @@
+'''import webbrowser
+webbrowser.open('https://www.google.com')
+'''
+import webbrowser
+
+url = 'http://docs.python.org/'
+
+# MacOS
+#chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+# Windows
+chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+
+webbrowser.get(chrome_path).open_new_tab(url)
